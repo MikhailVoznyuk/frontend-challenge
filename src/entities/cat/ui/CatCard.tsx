149ab03b0cf38756c288c9697bde7bb07ce4f9a2
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 02:59:19 +0300

    update: loaded store and added heart button to add image to favorites

--- src/entities/cat/ui/CatCard.tsx
+++ src/entities/cat/ui/CatCard.tsx
@@ -1,9 +1,17 @@
+import { useAppDispatch, useAppSelector } from "@/app/store/hooks.ts";
+import { selectIsFavorite } from "@/features/favorites/model/selectors.ts";
+import { toggleFavorite } from "@/features/favorites/model/favoritesSlice.ts";
+import { HeartButton } from "@/shared/ui/buttons";
+
 import type { Cat } from "@/entities/cat/model/types.ts";
 
 type Props = {
     cat: Cat
 }
 export function CatCard({cat}: Props) {
+    const dispatch = useAppDispatch();
+    const isFavorite = useAppSelector(selectIsFavorite(cat.id));
+
     return (
         <section className='relative w-56 h-56'>
             <img
@@ -14,6 +22,11 @@ export function CatCard({cat}: Props) {
                 width={cat.width}
                 height={cat.height}
                 className='w-full h-full object-cover object-center'
+            />
+            <HeartButton
+                isActive={isFavorite}
+                onClick={() => dispatch(toggleFavorite(cat))}
+                className='absolute right-4 bottom-4'
             />
         </section>
     )
