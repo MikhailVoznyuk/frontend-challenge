f71f29de0d43a145f1040a7aa314e766d9329f2f
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 02:58:56 +0300

    refactor

--- src/features/favorites/model/favoritesSlice.ts
+++ src/features/favorites/model/favoritesSlice.ts
@@ -16,7 +16,7 @@ const favoritesSlice = createSlice({
         setFavorites: (state, action: PayloadAction<Cat[]>) => {
             state.items = action.payload;
         },
-        toggleFavorites: (state, action: PayloadAction<Cat>) => {
+        toggleFavorite: (state, action: PayloadAction<Cat>) => {
             const cat = action.payload;
             const exists = state.items.some((item) => item.id === cat.id);
 
@@ -35,7 +35,7 @@ const favoritesSlice = createSlice({
 
 export const {
     setFavorites,
-    toggleFavorites,
+    toggleFavorite,
     removeFavorite,
     clearFavorites,
 } = favoritesSlice.actions;
