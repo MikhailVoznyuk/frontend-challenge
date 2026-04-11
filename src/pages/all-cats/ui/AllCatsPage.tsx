3e46a8440920c01fc2e639cc4ba577265bc39f72
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 01:20:17 +0300

    update: initialised store, added CatsGrid to the page

--- src/pages/all-cats/ui/AllCatsPage.tsx
+++ src/pages/all-cats/ui/AllCatsPage.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/store/hooks.ts";
+import { selectCats, selectCatsStatus } from "@/features/cats/model/selectors.ts";
+import { fetchCats } from "@/features/cats/model/catsSlice.ts";
+import { CatsGrid } from "@/widgets/cats-grid";
+
 export function AllCatsPage() {
+    const dispatch = useAppDispatch();
+    const cats = useAppSelector(selectCats);
+    const status = useAppSelector(selectCatsStatus);
+
+    useEffect(() => {
+        if (status === 'idle') {
+            dispatch(fetchCats(20));
+        }
+
+    }, [dispatch, status]);
+
     return (
-        <section>
-            <h2>Страница с котами</h2>
-        </section>
-    )
+        <CatsGrid variant='all' cats={cats} />
+    );
 }
