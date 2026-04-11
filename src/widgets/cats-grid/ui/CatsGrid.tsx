8082d86c6a782917264fc504597072a6ed85fbb1
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 01:19:03 +0300

    add: CatsGrid component

---
+++ src/widgets/cats-grid/ui/CatsGrid.tsx
@@ -0,0 +1,25 @@
+import {CatCard} from "@/entities/cat/ui/CatCard";
+import type {Cat} from "@/entities/cat/model/types";
+
+type Props = {
+    variant: 'all' | 'favorite'
+    cats: Cat[];
+}
+
+export function CatsGrid({cats, variant}: Props) {
+    if (!cats.length) {
+        return (
+            <div className=''>
+                {variant === 'all' ? 'Загружаю котиков...' : 'Здесь пока пусто...'}
+            </div>
+        )
+    }
+    return (
+        <section
+            className='w-full px-[62px] py-[52px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-[52px]'
+        >
+            {cats.map((cat) => <CatCard cat={cat} />)}
+        </section>
+    )
+
+}
