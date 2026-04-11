d80c7c85d289ed171f566168e133b1b5421028e4
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 01:18:36 +0300

    refactor

--- src/features/cats/model/selectors.ts
+++ src/features/cats/model/selectors.ts
@@ -1,5 +1,5 @@
 import type {RootState} from "@/app/store/index.ts";
 
 export const selectCats = (state: RootState) => state.cats.items;
-export const selectCatsStaus = (state: RootState) => state.cats.status;
+export const selectCatsStatus = (state: RootState) => state.cats.status;
 export const selectCatsError = (state: RootState) => state.cats.error;
