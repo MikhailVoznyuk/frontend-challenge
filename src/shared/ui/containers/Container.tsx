312da19daf6e263030fabddbdca88106f7feeeac
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 22:18:25 +0300

    update: changed vertical adaptive padding

--- src/shared/ui/containers/Container.tsx
+++ src/shared/ui/containers/Container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function Container({children}: Props) {
     return (
-        <div className='w-full flex flex-col items-center justify-center px-4 sm:px-8 md:px-[62px] py-4 sm:py-8 md:py-[52px] gap-8'>
+        <div className='w-full flex flex-col items-center justify-center px-4 sm:px-8 md:px-[62px] py-8 md:py-[52px] gap-8'>
             {children}
         </div>
     )
