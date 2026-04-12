a3f46a7c02527849ddef076998f9aac8d980e9d4
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 16:04:45 +0300

    update: new selectors

--- src/features/cats/model/selectors.ts
+++ src/features/cats/model/selectors.ts
@@ -3,3 +3,6 @@ import type {RootState} from "@/app/store/index.ts";
 export const selectCats = (state: RootState) => state.cats.items;
 export const selectCatsStatus = (state: RootState) => state.cats.status;
 export const selectCatsError = (state: RootState) => state.cats.error;
+export const selectCatsPage = (state: RootState) => state.cats.page;
+export const selectCatsHasMore = (state: RootState) => state.cats.hasMore;
+export const selectCatsFetchingMore = (state: RootState) => state.cats.isFetchingMore;
