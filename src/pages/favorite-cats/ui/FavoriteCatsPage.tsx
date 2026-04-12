42f3fc006b4a16cf21aba7c097ed072bc30732eb
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 16:05:28 +0300

    update: added empty state status bar

--- src/pages/favorite-cats/ui/FavoriteCatsPage.tsx
+++ src/pages/favorite-cats/ui/FavoriteCatsPage.tsx
@@ -1,10 +1,18 @@
 import { useAppSelector } from "@/app/store/hooks.ts";
 import { selectFavoriteCats } from "@/features/favorites/model/selectors.ts";
 import { CatsGrid } from "@/widgets/cats-grid";
+import { Container } from "@/shared/ui/containers";
+import { StatusBar } from "@/shared/ui/bars";
 
 export function FavoriteCatsPage() {
     const favoriteCats = useAppSelector(selectFavoriteCats);
     return (
-        <CatsGrid variant='favorite' cats={favoriteCats} />
+        <Container>
+            {
+                !favoriteCats.length &&
+                <StatusBar>Здесь пока пусто...</StatusBar>
+            }
+            <CatsGrid cats={favoriteCats} />
+        </Container>
     )
 }
