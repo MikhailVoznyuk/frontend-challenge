ec4bbfee7d288bb7f6fb0e1d8e4037df437cee12
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-04-12 16:04:19 +0300

    add: Container ui component

---
+++ src/shared/ui/containers/Container.tsx
@@ -0,0 +1,13 @@
+import {type ReactNode} from 'react'
+
+type Props = {
+    children: ReactNode
+}
+
+export function Container({children}: Props) {
+    return (
+        <div className='w-full flex flex-col items-center justify-center px-4 sm:px-8 md:px-[62px] py-4 sm:py-8 md:py-[52px] gap-8'>
+            {children}
+        </div>
+    )
+}
